a8af25850402ddfd4ffda6c1be35a284189839fb
Engjell Berisha <egiberisha9@gmail.com>
2025-10-03 14:10:13 +0200

    created add user page, json file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import UserList from "./pages/UserList";
 import UserDetails from "./pages/UserDetails";
-
+import AddUser from "./pages/AddUser"; 
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<UserList />} />
+        <Route path="/" element={<UserList users={users} />} />
         <Route path="/users/:id" element={<UserDetails />} />
       </Routes>
     </Router>
